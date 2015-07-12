36493dd4d1a773eee8c257260021e66abc02121b
William Arnold <Will748@gmail.com>
2015-07-12 09:27:04 -0700

    Correct service name

--- app/src/main/kotlin/com/aphoh/muser/network/SoundcloudService.kt
+++ app/src/main/kotlin/com/aphoh/muser/network/SoundcloudService.kt
@@ -8,7 +8,7 @@ import rx.Observable
 /**
  * Created by Will on 7/5/2015.
  */
-interface SoundCloudService {
+interface SoundcloudService {
     GET("/resolve.json")
     fun getSongFromUrl(Query("url") url: String, Query("client_id") clientId: String): Observable<Track>
 }
