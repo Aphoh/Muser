dc5c6f6284e728059279f5381ffbc2830666634a
William Arnold <Will748@gmail.com>
2015-07-25 09:43:23 -0700

    Start moving all playing logic into MusicInteractor

---
+++ app/src/main/java/com/aphoh/muser/base/MusicModule.java
@@ -0,0 +1,27 @@
+package com.aphoh.muser.base;
+
+import com.aphoh.muser.music.MusicInteractor;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Will on 7/25/15.
+ */
+@Module
+public class MusicModule {
+
+    private final BaseApplication appContext;
+
+    public MusicModule(BaseApplication appContext) {
+        this.appContext = appContext;
+    }
+
+    @Provides
+    @Singleton
+    public MusicInteractor provideMusicInteractor(){
+        return new MusicInteractor(appContext);
+    }
+}
---
+++ app/src/main/kotlin/com/aphoh/muser/music/MusicInteractor.kt
@@ -0,0 +1,14 @@
+package com.aphoh.muser.music
+
+import com.aphoh.muser.App
+import com.aphoh.muser.base.BaseApplication
+
+/**
+ * Created by Will on 7/25/15.
+ */
+public class MusicInteractor(val appContext : BaseApplication){
+
+
+
+
+}
