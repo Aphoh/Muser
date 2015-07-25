7c920504179a9da745b950a30b36654acd5ebffc
William Arnold <Will748@gmail.com>
2015-07-25 10:42:24 -0700

    Add testing dependencies and basic test

---
+++ app/src/test/java/com/aphoh/muser/test/MusicInteractorTest.kt
@@ -0,0 +1,23 @@
+package com.aphoh.muser.test
+
+import com.aphoh.muser.music.MusicInteractor
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by Will on 7/25/15.
+ */
+
+public class MusicInteractorTest(){
+    var musicInteractor : MusicInteractor? = null
+
+    Before
+    public fun setup(){
+        musicInteractor = MusicInteractor(null)
+    }
+
+    Test
+    public fun testSettingSongs(){
+
+    }
+}
