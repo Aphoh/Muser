948ff34c339ea4ccbed10b096805799ad2ca788d
Aphoh <will748@gmail.com>
2015-12-31 19:45:43 -0800

    Update reddit sccounter

--- reddit_testing/sccounter.py
+++ reddit_testing/sccounter.py
@@ -1,10 +1,18 @@
 import sys
-import requests
+import praw
 
 sub = sys.argv[1]
-r = requests.get("http://www.reddit.com/r/%s/top.json?t=week" % sub)
-result = r.json()['data']['children']
-size = len(
-	filter(lambda x:'soundcloud.com' in x['domain'],
-		map(lambda x:x['data'], result)))
-print size
+
+user_agent = ("PyMuserBot 0.1")
+r = praw.Reddit(user_agent=user_agent)
+
+subreddit = r.get_subreddit(sub)
+submissions = subreddit.get_top_from_week(limit=100)
+
+sc_links = 0
+
+for sub in submissions:
+	if("soundcloud.com" in sub.domain):
+		sc_links += 1
+
+print sc_links
