9bd8916bda7f8d3e9e68e6bfa197207729c14dc6
Aphoh <will748@gmail.com>
2016-01-19 21:48:37 -0800

    Revamp subreddit testing

--- reddit_testing/sccounter.py
+++ reddit_testing/sccounter.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import sys
+import csv
 import praw
 
 data = sys.argv[1]
@@ -8,26 +9,33 @@ user_agent = ("PyMuserBot 0.1")
 r = praw.Reddit(user_agent=user_agent)
 
 
-def sc_count(r, sub):
+def sc_count(r, subreddit_name):
 	try:
-		subreddit = r.get_subreddit(sub)
-		submissions = subreddit.get_top_from_week(limit=100)
+		subreddit = r.get_subreddit(subreddit_name)
+		submissions = subreddit.get_hot(limit=100)
 
 		sc_links = 0
+		scores = []
 
 		for sub in submissions:
 			if("soundcloud.com" in sub.domain):
 				sc_links += 1
-
-		return sc_links
+				scores.append(sub.score)
+		print subreddit_name
+		avg_score = if(len(scores) == 0) 0 else sum(scores)/len(scores)
+		return (subreddit_name, sc_links, avg_score)
 	except Exception, e:
-		return -1
+		print e
+		return ('error', 'error', 'error')
 	
 
 with open(data) as f:
+	result = open("result.csv", 'a')
+	writer = csv.writer(result)
+	writer.writerow(('Subreddit', 'Soundcloud Count', 'Average Score'))
 	for line in f:
 		sub = line.strip()
-		print "%d: %s" % (sc_count(r, sub), sub)
+		writer.writerow(sc_count(r, sub))
 
 
 
